9498bf44fb115dc5aebb45620960f997541f65cd
Julia Astakhova <walkaway1919@gmail.com>
2022-02-25 17:39:01 +0300

    add links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,13 +3,15 @@ import './Footer.scss';
 import { Link } from './Links/Links';
 import iconLinkedin from './Links/Icons/linkedIn_white.svg';
 import iconTelegram from './Links/Icons/telegram_white.svg'
+import iconGit from './Links/Icons/git.svg'
 
 export const Footer = ({className}) => {
     return <footer className={cn("footer", className)}>
         <p className="footer__message">Связаться со мной</p>
         <div className="footer__links">
-            <Link className='in' href="https://www.linkedin.com/in/%D1%8E%D0%BB%D0%B8%D1%8F-%D0%B0%D1%81%D1%82%D0%B0%D1%85%D0%BE%D0%B2%D0%B0-7b44681a9/" alt='linkedin' img={iconLinkedin}/>
+            <Link className='in' href="https://www.linkedin.com/in/walkaway1919" alt='linkedin' img={iconLinkedin}/>
             <Link className='tg' href="https://t.me/nightman_cometh" alt='telegram' img={iconTelegram}/>
+            <Link className='git' href="https://github.com/Walkaway1919" alt='git' img={iconGit}/>
         </div>
     </footer>
 }
